6bd746e71c9c0cee697d3c3081eb3a2d23bbc6a0
Gagan Aggarwal <62302397+pythoniseasy-hub@users.noreply.github.com>
2020-06-22 15:35:23 +0530

    Django installing on vitual env
    Django installing on vitual env

---
+++ LMSenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\gagan\OneDrive\Documents\Github\Library-Management-System\LMSenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
